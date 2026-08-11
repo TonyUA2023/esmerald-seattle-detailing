70978add92d0fcfcd659e9f807311aff5b07a4f9
TonyUA2023 <tomyulloa123@gmail.com>
2026-08-11 00:17:06 -0500

    implementacion seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-25270XMLSR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-25270XMLSR');
+          `}
+        </Script>
+        <Script
           id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
